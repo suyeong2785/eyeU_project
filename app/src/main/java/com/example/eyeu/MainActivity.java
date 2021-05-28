2a4b533d31dd466ee11c029799cc4f49ab8517bc
suyeong2785 <qkdtpwls_@naver.com>
2021-05-28 20:14:08 +0900

    노안검사 클릭시 화면전환

--- app/src/main/java/com/example/eyeu/MainActivity.java
+++ app/src/main/java/com/example/eyeu/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.eyeu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button oldEyeButton;
+    Button playButton;
+    Button imageButton;
+    Button lightButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        oldEyeButton = findViewById(R.id.oldEyeButton);
+        playButton = findViewById(R.id.playButton);
+        imageButton = findViewById(R.id.imageButton);
+        lightButton = findViewById(R.id.lightButton);
+
+        oldEyeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,oldeye_activity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
+
 }
